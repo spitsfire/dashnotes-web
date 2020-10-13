a3f1e5f30bde82263cecba39ba93689c2426279e
simon del rosario <simon@adadevelopersacademy.org>
2020-10-12 23:00:27 -0700

    Updates

---
+++ precache-manifest.ce5e51bd70339b6ec318d7d878c5ee98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b530377fc7698126f5b4fd969f97c5",
+    "url": "/dashnotes-web/index.html"
+  },
+  {
+    "revision": "83a625c55b554a671a6f",
+    "url": "/dashnotes-web/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6467f127af266335cc2c",
+    "url": "/dashnotes-web/static/js/2.f23ca445.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashnotes-web/static/js/2.f23ca445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83a625c55b554a671a6f",
+    "url": "/dashnotes-web/static/js/main.664dd219.chunk.js"
+  },
+  {
+    "revision": "3dea66039d8c67ff3506",
+    "url": "/dashnotes-web/static/js/runtime-main.5a894874.js"
+  }
+]);
